cb50e04091b4515c0989842fbbb3836a87ee16dd
unknown <luisgiav87@gmail.com>
2025-05-05 01:03:58 -0600

    new test

--- node/Connection/db.js
+++ node/Connection/db.js
@@ -9,20 +9,12 @@ const __dirname = path.dirname(__filename);
 const localDbPath = path.join(__dirname, "../../database/sqLite.db");
 const renderDbPath = "/data/sqLite.db";
 
-// Detectar entorno: si DATABASE_PATH_SAUL está definida, úsala
 const dbPath = process.env.DATABASE_PATH_SAUL || localDbPath;
 
 console.log(`Base de datos cargada desde: ${dbPath}`);
 
-// Si estamos en Render (detectando por si la ruta es /data/) y no existe el archivo, lo copiamos
+// Si estamos en Render (usando /data/sqLite.db) y no existe la base, la copia desde local
 if (dbPath === renderDbPath) {
-    // Verificar si existe la carpeta /data, si no la crea
-    if (!fs.existsSync("/data")) {
-        fs.mkdirSync("/data");
-        console.log("Carpeta /data creada.");
-    }
-
-    // Si no existe la base en /data, la copia desde local
     if (!fs.existsSync(renderDbPath)) {
         fs.copyFileSync(localDbPath, renderDbPath);
         console.log("Base de datos copiada a /data/sqLite.db");
